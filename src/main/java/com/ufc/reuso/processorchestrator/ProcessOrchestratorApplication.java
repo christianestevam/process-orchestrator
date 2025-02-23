01f717c75186744ab2b840d5e9250504c26c2173
Christian Estevam <christianestevamb@gmail.com>
2025-02-22 21:16:16 -0300

    init: setup project

---
+++ src/main/java/com/ufc/reuso/processorchestrator/ProcessOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.reuso.processorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufc/reuso/processorchestrator/ProcessOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.reuso.processorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
